51982f6939d90f7f54be310458bbb277d9b5a70b
Ilua Kisialiou <iluxa91@inbox.ru>
2022-06-04 23:05:08 +0300

    codwars

--- codewars.js
+++ codewars.js
@@ -3,3 +3,18 @@ function expressionMatter(a, b, c) {
 }
 
 console.log(expressionMatter(9,7,2))
+
+function feast(beast, dish) {return
+beast.slice(-1)==dish.slice(-1)&&beast[0]==dish[0]?true:false
+}
+
+function multiTable(number) {
+    return (`1 * ${number} = ${1 * number}\n`+`2 * ${number} = ${2 * number}\n`
+        +  `3 * ${number} = ${3 * number}\n` + `4 * ${number} = ${4 * number}\n`
+        + `5 * ${number} = ${5 * number}\n` +`6 * ${number} = ${6 * number}\n`
+        + `7 * ${number} = ${7 * number}\n` + `8 * ${number} = ${8 * number}\n` +
+        `9 * ${number} = ${9 * number}\n` + `10 * ${number} = ${10 * number}`
+    )
+}
+
+console.log(multiTable(5))
